a7c3fda79e4d4f3b8aec4ca8ed81fd8793613b3a
WU <aa70608726@gmail.com>
2019-09-04 22:18:12 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,10 @@ set -e
 npm run build
 # 移動至到打包後的dist目錄 
 ls
+pwd
 cd dist
 cd -
-git init # //因為dist資料夾預設是被ignore的，因此在進入dist資料夾後初始化git
+git init 
 git add -A
 git commit -m 'deploy'
 # 部署到 https://github.com/chou0728/eric-project.git 分支為 gh-pages
